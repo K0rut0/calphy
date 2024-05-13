e35974122cc93868f80c4b069404e488656325a3
K0rut0 <maleonkurt1@gmail.com>
2024-05-13 20:29:01 +0800

    fixed fma calculator

--- src/app/fma/page.tsx
+++ src/app/fma/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Card } from "@/components/ui/card";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 
 export default function force(){
     const massUnits = [
@@ -23,24 +23,111 @@ export default function force(){
     const [mass, setMass] = useState(0);
     const [acceleration, setAcceleration] = useState(0)
     const [force, setForce] = useState(0)
+    const [disable, setDisable] = useState(false)
+    const [lastChanges, setLastChanges] = useState<string[]>([])
+    function setMassValue(e : ChangeEvent<HTMLInputElement>){
+        if(e.target == null){
+            return;
+        }
+        const n = parseInt(e.target.value)
+        if(isNaN(n)){
+            setMass(0)
+            return;
+        }
+        if(lastChanges.length >= 2 && !lastChanges.includes("m")){
+            let temp = [...lastChanges]
+            temp.shift()
+            temp.push("m")
+            setLastChanges(temp)
+        } else if(!lastChanges.includes("m")){
+            let temp = [...lastChanges]
+            temp.push("m")
+            setLastChanges(temp)
+        }
+        setMass(n)
+    }
+    function setAccValue(e : ChangeEvent<HTMLInputElement>){
+        if(e.target == null){
+            return;
+        }
+        const n = parseInt(e.target.value)
+        if(isNaN(n)){
+            setAcceleration(0)
+            return;
+        }
+        if(lastChanges.length >= 2 && !lastChanges.includes("a")){
+            let temp = [...lastChanges]
+            temp.shift()
+            temp.push("a")
+            setLastChanges(temp)
+        } else if(!lastChanges.includes("a")){
+            let temp = [...lastChanges]
+            temp.push("a")
+            setLastChanges(temp)
+        }
+        setAcceleration(n)
+    }
+    function setForceValue(e : ChangeEvent<HTMLInputElement>){
+        if(e.target == null){
+            return;
+        }
+        const n = parseInt(e.target.value)
+        if(isNaN(n)){
+            setForce(0)
+            return;
+        }
+        if(lastChanges.length >= 2 && !lastChanges.includes("f")){
+            let temp = [...lastChanges]
+            temp.shift()
+            temp.push("f")
+            setLastChanges(temp)
+        } else if(!lastChanges.includes("f")){
+            let temp = [...lastChanges]
+            temp.push("f")
+            setLastChanges(temp)
+        }
+        setForce(n)
+    }
 
+    
+    function calculate(){
+        console.log(mass)
+        console.log(acceleration)
+        console.log(force)
+        console.log(lastChanges)
+        if(!lastChanges.includes("m")){
+            let n = force/acceleration
+            setMass(n)
+            return
+        }
+        if(!lastChanges.includes("a")){
+            let n = force/mass
+            setAcceleration(n)
+            return
+        }
+        if(!lastChanges.includes("f")){
+            let n = mass*acceleration
+            setForce(n)
+            return
+        }
+    }
     return(
         <div className="w-100 flex align-middle justify-center p-5">
             <Card className="flex w-[600px] h-[300px] border-black flex-col justify-center p-5 ">
                 <div className="flex flex-col w-100 gap-2 justify-center">
                     <div className="flex flex-col">
                         <label>Mass: </label>
-                        <input type="text" className="border-b-2 border-black" onChange={(e) => setMass(parseInt(e.target.value))} value={mass}></input>
+                        <input type="text" className="border-b-2 border-black" onChange={(e) => setMassValue(e)} value={mass}></input>
                     </div>
                     <div className="flex flex-col">
                         <label>Acceleration: </label>
-                        <input type="text" className="border-b-2 border-black" onChange={(e) => setAcceleration(parseInt(e.target.value))} value={acceleration}></input>
+                        <input type="text" className="border-b-2 border-black" onChange={(e) => setAccValue(e)} value={acceleration}></input>
                     </div>
                     <div className="flex flex-col">
                         <label>Force: </label>
-                        <input type="text" className="border-b-2 border-black" onChange={(e) => setForce(parseInt(e.target.value))} value={force}></input>
+                        <input type="text" className="border-b-2 border-black" disabled={disable} onChange={(e) => setForceValue(e)} value={force}></input>
                     </div>
-                    <button className="border-solid rounded-sm border border-black w-[100px]">Calculate</button>
+                    <button className="border-solid rounded-sm border border-black w-[100px]" onClick={calculate}>Calculate</button>
                 </div>
             </Card>
         </div>
